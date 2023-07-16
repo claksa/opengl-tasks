a5107dde7061d2e5ed73b7a0ad6bf64a4daf7f75
shimei <polina.pavlova19.10@gmail.com>
2023-07-16 16:45:41 +0300

    glad configured

--- main.cpp
+++ main.cpp
@@ -1,37 +1,13 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <cstdio>
-#include <cstdlib>
 
 
 int main() {
-    GLFWwindow *window;
-    int width, height;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    if( !glfwInit() )
-    {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
-        exit( EXIT_FAILURE );
-    }
-
-    glfwWindowHint(GLFW_DEPTH_BITS, 16);
-    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
-
-    window = glfwCreateWindow( 300, 300, "Gears", nullptr, nullptr );
-    if (!window)
-    {
-        fprintf( stderr, "Failed to open GLFW window\n" );
-        glfwTerminate();
-        exit( EXIT_FAILURE );
-    }
-
-    while( !glfwWindowShouldClose(window) )
-    {
-        // Swap buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    // Terminate GLFW
-    glfwTerminate();
     return 0;
 }
